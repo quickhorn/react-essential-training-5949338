1464a0d1e49168550804dca5b80d860b380bb800
quickhorn <quickhorn@gmail.com>
2025-03-05 21:34:28 +0000

    02_03b introduction to jsx

---
+++ react-project/react-project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+let language = "React";
+let moon = "🌙";
+
+function App() {
+  return (
+    <h1>
+      Hello {language.toUpperCase()} {moon}
+    </h1>
+  );
+}
+
+export default App;
