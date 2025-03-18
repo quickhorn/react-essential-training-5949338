bc27d8b49c60ba29301eea725eadad3b06526ff7
quickhorn <quickhorn@gmail.com>
2025-03-18 19:36:17 +0000

    0303 - items in a list and now in an object.

--- react-project/react-project/src/App.jsx
+++ react-project/react-project/src/App.jsx
@@ -1,13 +1,54 @@
 import "./App.css";
+import PropTypes from "prop-types";
 
-let language = "React";
-let moon = "🌙";
+function Header({ name, year }) {
+  return (
+    <header>
+      <h1>{name}&lsquo;s Kitchen</h1>
+      <p>Copyright {year}</p>
+    </header>
+  );
+}
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+};
+
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables",
+  "Minestrone Soup",
+];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish,
+}));
+
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li style={{ listStyleType: "none" }} key={dish.id}>
+          {dish.title}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+Main.propTypes = {
+  dishes: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 function App() {
   return (
-    <h1>
-      Hello {language.toUpperCase()} {moon}
-    </h1>
+    <div>
+      <Header name="Jenna" year={2025} />
+      <Main dishes={dishObjects} />
+    </div>
   );
 }
 
